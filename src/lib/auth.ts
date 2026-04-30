fe50deddc8de3fe1ee314450664900664448d21c
LnAPages <liftandalignwa@gmail.com>
2026-04-29 20:59:38 -0700

    Rename session cookies in clearSession function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 export function clearSession(): void {
-  document.cookie = 'fnlstg_session=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax';
+  document.cookie = 'LNAPAGES_session=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax';
   document.cookie = 'session_token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax';
-  document.cookie = 'fnlstg_csrf=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax';
+  document.cookie = 'LNAPAGES_csrf=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax';
 }
